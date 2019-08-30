f3173f9afedb6a548237d8123ceb8ee02385bd95
staman1702 <staman1702@gmail.com>
2019-08-30 13:13:18 +0200

    Project.Model added

---
+++ Project.Model/VehicleMake.cs
@@ -0,0 +1,11 @@
+﻿using Project.Model.Common;
+using System;
+using System.Collections.Generic;
+
+namespace Project.Model
+{
+    public class VehicleMake : Vehicle
+    {
+        public IList<VehicleModel> VehicleModels { get; set; } = new List<VehicleModel>();
+    }
+}
---
+++ Project.Model/VehicleModel.cs
@@ -0,0 +1,13 @@
+﻿using Project.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Model
+{
+    public class VehicleModel : Vehicle
+    {
+        public Guid VehicleMakeId { get; set; }
+        public VehicleMake VehicleMake { get; set; }
+    }
+}
