e34c09fdac0e50a82fef8d3f37c54798b1c52f76
staman1702 <staman1702@gmail.com>
2019-08-30 23:26:25 +0200

    added interfaces for both domain classes

---
+++ Project.Model.Common/IVehicleModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.Model.Common
+{
+    public interface IVehicleModel
+    {
+        Guid Id { get; set; }
+        string Name { get; set; }
+        string Abrv { get; set; }
+        Guid VehicleMakeId { get; set; }
+    }
+}
