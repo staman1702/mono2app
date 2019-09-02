98b2c973d8452b2fc86868843e30aff597d76c26
staman1702 <staman1702@gmail.com>
2019-09-02 12:25:00 +0200

    moved response class to project.api

---
+++ Project/Project.API/Communication/VehicleResponse.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project.API.Communication
+{
+    public class VehicleResponse<T> : BaseResponse
+    {
+        public T Response { get; set; }        
+
+
+        private VehicleResponse(bool success, string message, T item) : base(success, message)
+        {
+            Response = item;            
+        }
+
+        /// <param name="item">Saved item.</param>
+
+        public VehicleResponse(T item) : this(true, string.Empty, item)
+        { }
+
+
+        /// <param name="message">Error message.</param>
+
+        public VehicleResponse(string message) : this(false, message, default)
+        { }
+    }
+}
