f88bcf08bfc80ce01912da46cfb7f1bb5ce83f83
staman1702 <staman1702@gmail.com>
2019-09-05 19:43:45 +0200

    clean test repository

--- Project.Repository.Test/VehicleMakeRepositoryTest.cs
+++ Project.Repository.Test/VehicleMakeRepositoryTest.cs
@@ -8,23 +8,6 @@ namespace Project.Repository.Test
 {    
     public class VehicleMakeRepositoryTest
     {
-        //[Fact]
-        //public async Task ListMakeAsync_ShouldReturnAllVehicleMakesAsync()
-        //{          
-        //    var vehicleMakeRepository = new VehicleMakeRepository().ListMakeAsync;
-
-        //    var result = await vehicleMakeRepository.ListMakeAsync() as List<VehicleMake>;
-        //    Assert.AreEqual(testMakes.Count, result.Count);
-        //}
-
-        //private List<VehicleMake> GetTestMakes()
-        //{
-        //    var testMakes = new List<VehicleMake>();
-        //    testMakes.Add(new VehicleMake
-        //    { Id = Guid.Parse("215a2d92-c849-4a6d-817a-b385a1d7e975"), Name = "TestMakeName", Abrv = "TestMakeAbrv" });
-            
-
-        //    return testMakes;
-        //}
+        
     }
 }
