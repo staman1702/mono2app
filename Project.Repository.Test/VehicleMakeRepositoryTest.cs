7d95035a5016b5738a8ae70eb8644c9531840489
staman1702 <staman1702@gmail.com>
2019-09-05 19:42:05 +0200

    reorganized all project in one folder

---
+++ Project.Repository.Test/VehicleMakeRepositoryTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+using Moq;
+
+namespace Project.Repository.Test
+{    
+    public class VehicleMakeRepositoryTest
+    {
+        //[Fact]
+        //public async Task ListMakeAsync_ShouldReturnAllVehicleMakesAsync()
+        //{          
+        //    var vehicleMakeRepository = new VehicleMakeRepository().ListMakeAsync;
+
+        //    var result = await vehicleMakeRepository.ListMakeAsync() as List<VehicleMake>;
+        //    Assert.AreEqual(testMakes.Count, result.Count);
+        //}
+
+        //private List<VehicleMake> GetTestMakes()
+        //{
+        //    var testMakes = new List<VehicleMake>();
+        //    testMakes.Add(new VehicleMake
+        //    { Id = Guid.Parse("215a2d92-c849-4a6d-817a-b385a1d7e975"), Name = "TestMakeName", Abrv = "TestMakeAbrv" });
+            
+
+        //    return testMakes;
+        //}
+    }
+}
